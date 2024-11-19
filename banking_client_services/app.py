de6ba638ed456d9e7ef752f6896d3df8a24ce816
Marcos Pacheco <mcp_marcos@hotmail.com>
2024-11-18 23:36:14 -0300

    Post user route created.

--- banking_client_services/app.py
+++ banking_client_services/app.py
@@ -1,8 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI 
+from http import HTTPStatus
+from banking_client_services.Schemas import UserSchema
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", status_code=HTTPStatus.CREATED)
 def readRoot():
     return {"message": "Hello World"}
+ 
+
+@app.post("/user/", response_Model=UserSchema)
+def create_user(user: UserSchema):
+    return user
+ 
+ 
+ # read
+ # update
+ # delete
