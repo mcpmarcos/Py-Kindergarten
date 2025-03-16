a2a66161087f93c9b3ba2f07ae0c698b8ae8d87e
mcpmarcos <mcp_marcos@hotmail.com>
2025-03-16 00:36:41 -0300

    study/executando-queries

--- sqla/01sqla_sync/select_main.py
+++ sqla/01sqla_sync/select_main.py
@@ -42,6 +42,7 @@ def select_filtro_sabor(id_sabor: int) -> None:
         # sabor: Sabor = session.query(Sabor).filter(Sabor.id == id_sabor).one()
         
         #forma 4 usando where() ao invés de filter(), e com eleé possível usar o one(), one_or_none() e first()
+        # o sql alchemy perque seja usada mais de uma condição dentro do where() e também dentro do order_by()
         sabor: Sabor = session.query(Sabor).where(Sabor.id == id_sabor).one()
 
         print(f'ID: {sabor.id}')
@@ -73,7 +74,69 @@ def select_complexo_picole() -> None:
 
             print('--------------------------------------')
 
+def select_order_by_sabor() -> None:
+    with create_session() as session:
+        sabores: List[Sabor] = session.query(Sabor).order_by(Sabor.data_criacao.desc()).all()
+        
+        for sabor in sabores:
+            print(f'ID: {sabor.id}')
+            print(f'Nome: {sabor.nome}')
+            print('--------------------------------------')
+
+
+def select_group_by_picole() -> None:
+    with create_session() as session:
+        picoles: List[Picole] = session.query(Picole).group_by(Picole.id, Picole.id_tipos_picole).all()
+
+        for picole in picoles:
+            print(f'ID: {picole.id}')
+            print(f'Preço: {picole.preco}')
+            print(f'Sabor: {picole.sabor.nome}')
+            print(f'id tipo_picole: {picole.id_tipos_picole}')
+            print('--------------------------------------')
+
+def select_limit() -> None:
+    with create_session() as session:
+        sabores: List[Sabor] = session.query(Sabor).limit(25)
+        for sabor in sabores:
+            print(f'ID: {sabor.id}')
+            print(f'Nome: {sabor.nome}')
+            print('--------------------------------------')
+
+def select_count() -> None:
+    with create_session() as session:
+        qtd: int = session.query(Revendedor).count()
+        print(f'Quantidade de revendedores: {qtd}')
+        print('--------------------------------------')
+
+def select_agregacao() -> None:
+    with create_session() as session:
+        result: List = session.query(
+            func.sum(Picole.preco).label('soma'),
+            func.avg(Picole.preco).label('media'),
+            func.min(Picole.preco).label('minimo'), 
+            func.max(Picole.preco).label('maximo')
+            
+        ).all()
+        print(f'A soma de todos os picolés é: {result[0][0]}')
+        print(f'A média de todos os picolés é: {result[0][1]}')
+        print(f'O menor preço de todos os picolés é: {result[0][2]}')
+        print(f'O maior preço de todos os picolés é: {result[0][3]}')
+
+        print('--------------------------------------')
+
+        print(f'Resultado: {result}')
+
+        print('--------------------------------------')
+
+
+
 if __name__ == '__main__':
     #select_todos_aditivos_nutritivos()
     #select_filtro_sabor(21)
-    select_complexo_picole()
+    # select_complexo_picole()
+    # select_order_by_sabor()
+    # select_group_by_picole()
+    # select_limit()
+    # select_count()
+    select_agregacao()
